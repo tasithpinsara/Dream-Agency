c1ebd2d49c73c01618bfe0aa27861b6bdf4c5df4
tasithpinsara <pinsaramahakumara.pesonl@gmail.com>
2023-12-08 10:16:53 -0800

    new update fill add

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+
+let chard = document.querySelector(".boxes");
+let next_btn = document.getElementById("next-btn");
+let back_btn = document.getElementById("back-btn");
+
+
+chard.addEventListener("wheel", (evt) => {
+evt.preventDefault();
+chard.scrollLeft += evt.deltaY;
+chard.style.scrollBehavior = "auto";
+});
+
+next_btn.addEventListener("click", ()=>{
+  chard.style.scrollBehavior = "smooth";
+  chard.scrollLeft += 900;
+  
+});
+
+back_btn.addEventListener("click", ()=>{
+  chard.style.scrollBehavior = "smooth";
+  chard.scrollLeft -= 900;
+  
+});
+
+
+
